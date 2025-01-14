811d2df1e3621a227c3201712c01d871b77635e1
Brandon Symmers <brandon@Brandons-MacBook-Air.local>
2025-01-13 20:12:33 -0800

    Added Database Connection Postgres DB Integration & Skeleton for some post endpoints.

--- src/api/router.go
+++ src/api/router.go
@@ -1,12 +1,16 @@
 package api
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 type Book struct {
 	ISBN        string  `json:"isbn"`
 	Title       string  `json:"title"`
 	Author      string  `json:"author"`
-	Date        float64 `json:"published_date"`
+	Date        string  `json:"published_date"`
 	Edition     float64 `json:"edition"`
 	Genre       string  `json:"genre"`
 	Description string  `json:"description"`
@@ -22,8 +26,6 @@ type Collection struct {
 
 func NewRouter() {
 	router := gin.Default()
-	router.Run("localhost:8080")
-
 	router.GET("/books", getBooks)
 	router.POST("/books", postBooks)
 	router.DELETE("/books", deleteBooks)
@@ -31,34 +33,60 @@ func NewRouter() {
 	router.POST("/collections", postCollections)
 	router.DELETE("/collections", deleteCollections)
 	router.PUT("/collections", putCollections)
+	router.Run("localhost:8080")
+}
+
+func processor(c *gin.Context, incomingObj any) bool {
+	if err := c.ShouldBind(&incomingObj); err != nil {
+		c.String(http.StatusBadRequest, "bad request: %v", err)
+		return false
+	}
+	return true
 }
 
 // Book Endpoints
 func getBooks(c *gin.Context) {
+	//TODO: check if isbn is in db
 
 }
 
 func postBooks(c *gin.Context) {
+	var newBook Book
+
+	isValid := processor(c, newBook)
+	if !isValid {
+		return
+	}
 
 }
 
 func deleteBooks(c *gin.Context) {
+	// TODO: check if isbn is in db
 
 }
 
 // Collection Endpoints
 func getCollections(c *gin.Context) {
+	// TODO: check if name is in db
 
 }
 
 func postCollections(c *gin.Context) {
+	var newCollection Collection
+
+	isValid := processor(c, newCollection)
+	if !isValid {
+		return
+	}
 
 }
 
 func deleteCollections(c *gin.Context) {
+	// TODO: check if name is in db
 
 }
 
 func putCollections(c *gin.Context) {
+	// TODO: check if name is in db and if book isbn is in db
 
 }
